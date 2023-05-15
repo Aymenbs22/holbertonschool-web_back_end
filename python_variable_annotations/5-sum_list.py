cbdb7d3e2081711532999600414898cfbba33312
root <aymenbenslima1@gmail.com>
2023-05-15 18:59:32 +0000

    function sum_list which takes a list input_list of floats as argument and returns their sum as a float

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -9,6 +9,6 @@ from typing import List, Set, Dict, Tuple
 
 
 def sum_list(input_list: List[float]) -> float:
-"""function sum_list which takes a list input_list of floats
+    """function sum_list which takes a list input_list of floats
 as argument and returns their sum as a float"""
     return(math.fsum(input_list))
