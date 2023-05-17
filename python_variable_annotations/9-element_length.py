d29c8c5de963f21670f24bb49294644b95b0a8d9
root <aymenbenslima1@gmail.com>
2023-05-17 01:01:46 +0000

    function’s parameters and return values with the appropriate types

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""function’s parameters and return values
+with the appropriate types"""
+
+
 import math
 import typing
 from typing import List, Set, Dict, Tuple, Union, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """function’s parameters and return values
+with the appropriate types"""
     return [(i, len(i)) for i in lst]
