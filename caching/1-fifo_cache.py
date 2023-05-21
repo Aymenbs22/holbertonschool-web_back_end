837f574ae314da5d041342d7e3354036560bfab7
root <aymenbenslima1@gmail.com>
2023-05-21 17:52:55 +0000

    Must assign to the dictionary self.cache_data the item value for the key key.

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -7,6 +7,7 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """class FIFOCache that inherits from BaseCaching and is a caching system"""
     def __init__(self):
         """ Initiliaze
         """
