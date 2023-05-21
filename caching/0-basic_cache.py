7aeaf8af89a5e1ce76a8e30f9b258c7f16cbc733
root <aymenbenslima1@gmail.com>
2023-05-21 14:04:59 +0000

    class BasicCache that inherits from BaseCaching and is a caching system:

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -6,6 +6,8 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """class BasicCache that inherits from
+    BaseCaching and is a caching system"""
     def put(self, key, item):
         """Must assign to the dictionary self.cache_data
         the item value for the key key.
