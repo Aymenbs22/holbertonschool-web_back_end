aeef4ac408f773883a6193ad4b2733c8f7b8fb5a
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-28 16:21:35 +0100

    Implement a method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10.
    You have to use this CSV file (same as the one presented at the top of the project)
    Use assert to verify that both arguments are integers greater than 0.
    Use index_range to find the correct indexes to paginate the dataset correctly and return the appropriate page of the dataset (i.e. the correct list of rows).
    If the input arguments are out of range for the dataset, an empty list should be returned

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -10,6 +10,8 @@ from typing import List
 
 
 def index_range(page: int, page_size: int) -> Tuple:
+    """function named index_range that takes
+two integer arguments page and page_size"""
     start = page_size * (page - 1)
     end = start + page_size
     return start, end
