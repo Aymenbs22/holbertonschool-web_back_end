57e7d3b60943d12d28cc84555f1888b326250ccb
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-28 23:22:13 +0100

    Implement a get_hyper_index method with two integer arguments: index with a None default value and page_size with default value of 10.
    The method should return a dictionary with the following key-value pairs:
    index: the current start index of the return page. That is the index of the first item in the current page. For example if requesting page 3 with page_size 20, and no data was removed from the dataset, the current index should be 60.
    next_index: the next index to query with. That should be the index of the first item after the last item on the current page.
    page_size: the current page size
    data: the actual page of the dataset

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -9,6 +9,8 @@ from typing import List, Set, Dict, Tuple, Union, Callable
 
 
 def index_range(page: int, page_size: int) -> Tuple:
+    """function named index_range that takes
+two integer arguments page and page_size"""
     start = page_size * (page - 1)
     end = start + page_size
     return start, end
