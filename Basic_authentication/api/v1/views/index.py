83dbe8be402e809ac52305c131c5750f90733021
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-30 23:26:41 +0100

    For testing this new error handler, add a new endpoint in api/v1/views/index.py:
    Route: GET /api/v1/forbidden
    This endpoint must raise a 403 error by using abort - Custom Error Pages
    By calling abort(403), the error handler for 403 will be executed

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -22,6 +22,14 @@ def unauthorized():
     return abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """
+    Add a new error handler for this status code
+    """
+    return abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
