5b0b5baf3e393ad24ddfa9df4baeb90c0f841cad
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-30 23:40:39 +0100

    create a class to manage the API authentication.
    Create a folder api/v1/auth
    Create an empty file api/v1/auth/__init__.py
    Create the class Auth:
    in the file api/v1/auth/auth.py
    import request from flask
    class name Auth
    public method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: that returns False - path and excluded_paths will be used later, now, you don’t need to take care of them
    public method def authorization_header(self, request=None) -> str: that returns None - request will be the Flask request object
    public method def current_user(self, request=None) -> TypeVar('User'): that returns None - request will be the Flask request object

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+"""create a class to manage the API authentication"""
 
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
+    """Auth Vlass"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method def require_auth"""
         return False
-    
 
     def authorization_header(self, request=None) -> str:
+        """public method def authorization_header"""
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """public method def current_user"""
         return None
