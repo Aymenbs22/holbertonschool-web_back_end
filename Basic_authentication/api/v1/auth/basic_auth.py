10b602c606b0228d8af16fef7f2a3b0cf5a55131
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-31 20:49:31 +0100

    method def extract_base64_authorization_header(self, authorization_header: str) -> str: in the class BasicAuth that returns the Base64 part of the Authorization header for a Basic Authentication:
    Return None if authorization_header is None
    Return None if authorization_header is not a string
    Return None if authorization_header doesn’t start by Basic (with a space at the end)
    Otherwise, return the value after Basic (after the space)
    You can assume authorization_header contains only one Basic

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,6 +12,8 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """method in the class BasicAuth that returns the Base64
+        part of the Authorization header for a Basic Authentication"""
         if authorization_header is None:
             return None
         if type(authorization_header) != str:
