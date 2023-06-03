316749bcfac0a3b57a96f0c3b161850954f32498
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-03 19:09:46 +0100

    Update the @app.before_request method in api/v1/app.py:
    Add the URL path /api/v1/auth_session/login/ in the list of excluded paths of the method require_auth - this route doesn’t exist yet but it should be accessible outside authentication
    If auth.authorization_header(request) and auth.session_cookie(request) return None, abort(401)

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -56,9 +56,10 @@ def before_request():
     if auth is None:
         return None
     pathlist = ['/api/v1/status/', '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, pathlist):
-        if not auth.authorization_header(request):
+        if not auth.authorization_header(request) \
+           and not auth.session_cookie(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
