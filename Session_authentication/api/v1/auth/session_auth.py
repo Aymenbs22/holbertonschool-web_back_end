e6866d8d16dd8366c348573c7060e0638075f4fb
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-03 02:06:57 +0100

    instance method def user_id_for_session_id(self, session_id: str = None) -> str: that returns a User ID based on a Session ID:
    Return None if session_id is None
    Return None if session_id is not a string
    Return the value (the User ID) for the key session_id in the dictionary user_id_by_session_id.
    You must use .get() built-in for accessing in a dictionary a value based on key
    Now you have 2 methods (create_session and user_id_for_session_id) for storing and retrieving a link between a User ID and a Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,13 @@ class SessionAuth(Auth):
         SessionID = str(uuid.uuid4())
         self.user_id_by_session_id[SessionID] = user_id
         return SessionID
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """instance method that returns a
+        User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if type(session_id) != str:
+            return None
+        userid = self.user_id_by_session_id.get(session_id)
+        return userid
