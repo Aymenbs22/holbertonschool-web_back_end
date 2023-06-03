077b920f4aafbf57bd0db84079daa4a6c20db86f
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-03 01:03:14 +0100

    Create a class attribute user_id_by_session_id initialized by an empty dictionary Create an instance method def create_session(self, user_id: str = None) -> str: that creates a Session ID for a user_id: Return None if user_id is None Return None if user_id is not a string Otherwise: Generate a Session ID using uuid module and uuid4() like id in Base Use this Session ID as key of the dictionary user_id_by_session_id - the value for this key must be user_id Return the Session ID The same user_id can have multiple Session ID - indeed, the user_id is the value in the dictionary user_id_by_session_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,19 @@ Route module for the API
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """class SessionAuth that inherits from Auth"""
-    def __init__(self):
-        return
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """instance method that creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+        if type(user_id) != str:
+            return None
+        SessionID = str(uuid.uuid4())
+        self.user_id_by_session_id[SessionID] = user_id
+        return SessionID
+        
