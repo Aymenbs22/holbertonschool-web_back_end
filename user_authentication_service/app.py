84c72fc1c0bcabcc86cd18585e8340d02c58b7fc
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-05 18:43:28 +0100

    Flask app that has a single GET route (/) and use flask.jsonify to return a JSON payload of the form

---
+++ user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET'])
+def hello_world():
+    return jsonify(
+        greeting=["hello", "world"],
+        date=date.today(),
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
