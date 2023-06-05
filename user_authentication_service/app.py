92ca6649b3daf0553b51f4a68881dbd56c289315
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-05 19:40:48 +0100

    The end-point should expect two form data fields: email and password. If the user does not exist, the end-point should register it and respond with the following JSON payload

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
 def hello_world():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def Register():
+    """If the user does not exist, the end-point
+    should register it and respond"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "<registered email>",
+                        "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
