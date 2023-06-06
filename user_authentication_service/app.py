0356b33a31fe67e4031d1ff40f4d8cad9868cbec
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-06 03:24:57 +0100

    The request is expected to contain the session ID as a cookie with key session_id.
    Find the user with the requested session ID. If the user exists destroy the session and redirect the user to GET /. If the user does not exist, respond with a 403 HTTP status

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -43,6 +43,19 @@ def Login():
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'])
+def Logout():
+    """request is expected to contain the
+    session ID as a cookie with key session_id"""
+    requestcookies = request.cookies.get("session_id")
+    findeduser = AUTH.get_user_from_session_id(requestcookies)
+    if not findeduser:
+        abort(403)
+    else:
+        AUTH.destroy_session(findeduser)
+        redirect("/")
 
 
 if __name__ == "__main__":
