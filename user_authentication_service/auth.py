0f73f55dc5baf60b98c7e6957b76fda203dbf1b0
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-06 01:30:48 +0100

    If the session ID is None or no user is found, return None. Otherwise return the corresponding user.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -66,3 +66,12 @@ class Auth:
             return uuid
         except Exception:
             return
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """If the session ID is None or no user is found,
+        return None. Otherwise return the corresponding user."""
+        if not session_id:
+            return None
+        else:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
