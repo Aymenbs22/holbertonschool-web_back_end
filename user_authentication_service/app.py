9e4ad999cd3b08eab51e03fe65975c2e7b83779c
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-06 01:14:05 +0100

    The request is expected to contain form data with email and a password fields.
    If the login information is incorrect, use flask.abort to respond with a 401 HTTP status.
    
    Otherwise, create a new session for the user, store it the session ID as a cookie with key session_id on the response and return a JSON payload of the form

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -20,10 +20,29 @@ def Register():
     password = request.form.get('password')
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": "<registered email>",
+        return jsonify({"email": email,
                         "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'])
+def Login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email:
+        return None
+    if not password:
+        return None
+    valid_login = AUTH.valid_login(email, password)
+    if valid_login:
+        newses = AUTH.create_session(email)
+        response = make_response()
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", newses)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
