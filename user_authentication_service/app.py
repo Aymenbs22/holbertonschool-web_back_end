f0984ca0b59deccdfc371fcbab7cdb56ca7788cc
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-07 02:29:07 +0100

    The request is expected to contain form data with fields email, reset_token and new_password.
    Update the password. If the token is invalid, catch the exception and respond with a 403 HTTP code.
    
    If the token is valid, respond with a 200 HTTP code and the following JSON payload

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -82,5 +82,18 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route("/reset_password", methods=['PUT'])
+def update_password():
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+        reset = AUTH.update_password(reset_token, new_password)
+        return reset and jsonify({"email": email,
+                                  "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
