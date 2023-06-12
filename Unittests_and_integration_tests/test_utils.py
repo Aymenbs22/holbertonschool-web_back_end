64ed86e5ea28121a89ffb6dc0d18b1ef86a579c8
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-12 20:59:14 +0100

    Familiarize yourself with the utils.get_json function.
    Define the TestGetJson(unittest.TestCase) class and implement the TestGetJson.test_get_json method to test that utils.get_json returns the expected result.
    
    We don’t want to make any actual external HTTP calls. Use unittest.mock.patch to patch requests.get. Make sure it returns a Mock object with a json method that returns test_payload which you parametrize alongside the test_url that you will pass to get_json with the following inputs

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 from parameterized import parameterized
 import unittest
 import utils
+from utils import get_json
 from unittest.mock import Mock, patch
 
 
@@ -27,3 +28,19 @@ class TestAccessNestedMap(unittest.TestCase):
         KeyError is raised for the following inputs"""
         with self.assertRaises(KeyError):
             self.assertEqual(utils.access_nested_map(nested_map, path))
+
+
+class TestGetJson(unittest.TestCase):
+    """class and implement the TestGetJson.test_get_json
+    method to test that utils.get_json
+    returns the expected result"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_payload, test_url):
+        mockresponse = Mock()
+        mockresponse.json = Mock(return_value=test_payload)
+        with patch('requests.get', return_value=mockresponse):
+            json_data = get_json(test_url)
+            self.assertEqual(json_data, test_payload)
