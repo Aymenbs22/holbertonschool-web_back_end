47719db20d00260fbaadb05f05072e1e1eef3f48
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-13 01:23:24 +0100

    Implement TestGithubOrgClient.test_public_repos to unit-test GithubOrgClient.public_repos.
    Use @patch as a decorator to mock get_json and make it return a payload of your choice.
    
    Use patch as a context manager to mock GithubOrgClient._public_repos_url and return a value of your choice.
    
    Test that the list of repos is what you expect from the chosen payload.
    
    Test that the mocked property and the mocked get_json was called once

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -21,12 +21,16 @@ class TestGithubOrgClient(unittest.TestCase):
         mock.assert_called_once_with("https://api.github.com/orgs/" + org)
 
     def test_public_repos_url(self):
+        """test_public_repos_url method to unit-test
+        GithubOrgClient._public_repos_url"""
         with patch("client.GithubOrgClient.org") as mock:
             playload = {"repos_url": "a"}
             mock.return_value = playload
 
     @patch('client.get_json')
     def test_public_repos(self, mock):
+        """Test that the mocked property and
+        the mocked get_json was called once"""
         with patch("client.GithubOrgClient._public_repos_url") as mock:
             playload = {"repos_url": "a"}
             mock.return_value = playload
