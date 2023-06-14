966829e7730113446939f8d242971332e88c8580
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-14 01:36:10 +0100

    mock a database user table. Logging in will be mocked by passing login_as URL parameter containing the user ID to log in as.
    Define a get_user function that returns a user dictionary or None if the ID cannot be found or if login_as was not passed.
    
    Define a before_request function and use the app.before_request decorator to make it be executed before all other functions. before_request should use get_user to find a user if any, and set it as a global on flask.g.user.
    
    In your HTML template, if a user is logged in, in a paragraph tag, display a welcome message otherwise display a default message as shown in the table below

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, render_template, g, request
-from flask_babel import Babel, request
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -59,3 +59,6 @@ def before_request():
     """function and use the app.before_request decorator
     to make it be executed before all other functions"""
     g.user = get_user()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
