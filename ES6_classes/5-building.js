edbbb8b2727f92578f4b21f29ef95ae0dcee738d
Aymenbs22 <aymenbenslima1@gmail.com>
2023-07-15 08:10:06 +0000

    method named evacuationWarningMessage and return the following string Evacuate slowly the NUMBER_OF_FLOORS floors

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,12 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw Error('Class extending Building must override evacuationWarningMessage');
+      throw Error new('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
   get sqft() {
-    return this.sqft;
+    return this._sqft;
   }
 }
