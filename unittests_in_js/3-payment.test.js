b43cfa6358e07a9ee680b1a448ddb393db8049e1
Aymenbs22 <aymenbenslima1@gmail.com>
2023-07-30 22:27:18 +0000

    used for sendPaymentRequestToApi(100, 20) is the same as Utils.calculateNumber('SUM', 100, 20) (validate the usage of the Utils function)

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var sinon = require("sinon");
+  
+const calculateNumber = require("./3-payment.js");
+
+
+  describe('sum', () => {
+    it(`...`, () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      });
+  });
+
+
+  describe('SUBTRACT', () => {
+      it(`...`, () => {
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+      });
+  });
+
+          
+  describe('DIVIDE', () => {
+    it(`...`, () => {
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        expect(calculateNumber('DIVIDE', 10.3, 0)).to.equal('Error');
+        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+      });    
+  });
